c4bc815a3539e72b48e3d0fdcbd203e189452ffb
Anton Kechashin <atk@sitecore.net>
2017-06-21 10:47:21 +0300

    Initial commit.

---
+++ src/Sitecore.Support.108874/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.108874")]
+[assembly: AssemblyProduct("Sitecore.Support.108874")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.108874/sitecore/login/Default.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.sitecore.login
+{
+  using Sitecore.Web.Authentication;
+  using System;
+  public class Default : Sitecore.sitecore.login.Default
+  {
+    protected override void OnInit(EventArgs e)
+    {
+      #region Added code
+      if (!Sitecore.Context.User.IsAuthenticated || DomainAccessGuard.GetAccess())
+      {
+        base.Response.Headers.Add("finalURL", Globals.ServerUrl + Sitecore.Context.RawUrl);
+      } 
+      #endregion
+      base.OnInit(e);
+    }
+  }
+}
